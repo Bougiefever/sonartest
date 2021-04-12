e1e316cee2bb68527bc31640211a8f8ca2d0294c
bougiefever <anbougie@microsoft.com>
2021-04-12 09:35:41 -0500

    add zero test

--- badmath.cs
+++ badmath.cs
@@ -4,10 +4,10 @@ public class BadMath : IDisposable
 {
 
     public int factorial(int x) {
-        // if (x == 0)
-        //     return 1;
-        // else 
-        return x * factorial(x - 1);
+        if (x == 0)
+            return 1;
+        else 
+            return x * factorial(x - 1);
     }
 
     public void Dispose()
